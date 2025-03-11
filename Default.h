cd40932cbf73ad2b8108c271b9b0d10caafe5479
TVAn-yeet <24020005@vnu.edu.vn>
2025-03-11 22:51:29 +0700

    Delete Default.h

--- Default.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-const int WINDOW_HEIGHT = 600;
-const int WINDOW_WIDTH = 800;
-const int TILE_SIZE = 40;
-const int MAP_WIDTH = WINDOW_WIDTH / TILE_SIZE;
-const int MAP_HEIGHT = WINDOW_HEIGHT / TILE_SIZE;
-const int NUM_ROCK = 30;
