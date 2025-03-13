d1a7d9c60e60412978adc7e31409dc6bfd77e260
TVAn-yeet <24020005@vnu.edu.vn>
2025-03-13 18:09:20 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,20 +6,53 @@
 #include "End.h"
 using namespace std;
 void run() {
+    startTime = SDL_GetTicks();
+
     while (running) {
-        
+        Uint32 currentTime = SDL_GetTicks();
+        float deltaTime = (currentTime - lastTime) / 1000.0f;
+        lastTime = currentTime;
+
+        Uint32 elapsedTime = currentTime - startTime; 
+        int seconds = elapsedTime / 1000;
+        int nextsec = 0;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
         if (inMenu == true && ended == false) {
             handleMenuEvents();
             renderMenu();
         }
         else {
             if (!ended) {
-                Uint32 currentTime = SDL_GetTicks();
-                float deltaTime = (currentTime - lastTime) / 1000.0f;
-                lastTime = currentTime;
+               
+                
                 handleGameEvents(deltaTime);
                 renderGame();
                 Checkend();
+                if (moveable) {
+
+                    rab.move(deltaTime, fox.x, fox.y, chose);
+                    if (chose ==1||chose==3) {
+                        if((nextY - rab.y) < 6 && (nextY - rab.y) > -6)
+                            moveable = false;
+
+                        
+                    }
+                    else {
+                        if ((nextX - rab.x) < 6 && (nextX - rab.x) > -6)
+                            moveable = false;
+                    }
+                    for (int i : mov) {
+                        cout << i << " ";
+                    }
+                    cout <<chose<<' ';
+                    cout << endl;
+                }
+                else {
+                    
+                    rab.checkmove(fox.x, fox.y, chose, mov, nextX, nextY, moveable);
+                    
+                }
             }
             else {
                 if (win) {
